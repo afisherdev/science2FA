71813fba65b997dc63a52e59511b73f67e00cf6c
Andrew Fisher <development@Andrews-MacBook-Pro.local>
2018-07-29 12:29:59 -0700

    new stuff

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,13 +1,7 @@
-
-
 module.exports = function(app) {
-  app.get('/jeffrey', requireAuth, function(req, res) {
-    res.send({ message: 'Super secret code is ABC123' });
+  app.get("/verify/:verID");
+
+  app.get("*", function(req, res) {
+    res.sendFile("index.html", { root: "../client/build" });
   });
-  app.get('/verify/:verID' args)
-  app.post('/auth/signin', requireSignin, Authentication.signin);
-  app.post('/auth/signup', Authentication.signup);
-  app.get('*',  function(req, res) {
-    res.sendFile('index.html', { root: '../client/build' });
-});
-}
+};
--- server/server.js
+++ server/server.js
@@ -1,26 +1,22 @@
 //===Package  Requirements===
 const express = require("express");
-const mongoose = require("mongoose");
 const http = require("http");
-const morgan = require("morgan");
+
 const cors = require("cors");
-const jwt = require("jwt-simple");
+
 // const cookieSession = require('cookie-session');
-const passport = require("passport");
-const keys = require("./config/keys");
+
 const path = require("path");
 const bodyParser = require("body-parser");
-const validator = require("express-validator");
 
-require("ethereum.js");
+const twofactor = require("./ethereum.js");
 
 //========App Setup======
 const app = express();
 app.use(cors());
-app.use(morgan("combined"));
+twofactor.ethereum();
 app.use(bodyParser.json({ type: "*/*" }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(validator());
 app.use(express.static(path.join(__dirname, "client/build")));
 
 require("./routes/router")(app);
